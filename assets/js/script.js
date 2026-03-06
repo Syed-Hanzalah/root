325f1d34ab68476f648803493d748a357d042631
Syed-Hanzalah <142049263+Syed-Hanzalah@users.noreply.github.com>
2026-03-06 21:41:46 +0500

    one complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,14 @@ const sidebar = document.querySelector(".sidebar");
 chevron.addEventListener("click", () => {
     sidebar.classList.toggle("close");
 });
+const menuBtn = document.querySelector(".menu-toggle");
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+});
+document.addEventListener("click", function(e){
+
+if(!sidebar.contains(e.target) && !menuBtn.contains(e.target)){
+sidebar.classList.remove("active");
+}
+
+});
